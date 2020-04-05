1331d445aafe3ba9dae67fc858eb6a140f1e2961
TL Chang <coooo77@hotmail.com>
2020-04-05 13:49:24 +0800

    Add index page

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,16 @@ const router = express.Router()
 const Url = require('../models/url')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  Url.find()
+    .lean()
+    .exec((err, urls) => {
+      const mainUrl = 'http://localhost:3000/'
+      for (let i = 0; i < urls.length; i++) {
+        urls[i].paramsUrl = mainUrl + urls[i].paramsUrl
+      }
+      if (err) return console.error(err)
+      res.render('index', { urls, mainUrl })
+    })
 })
 
 module.exports = router
