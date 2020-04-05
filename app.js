50144af1a2dbfcd5e844c03d1e7ccaf7973db7ea
TL Chang <coooo77@hotmail.com>
2020-04-05 20:34:51 +0800

    Add input router

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000 // = process.env
 
+// bodyParser要放在express-handlebars之前才會有作用
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/urlShortener', {
@@ -21,6 +22,22 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('mongodb connected!')
+})
+
+const session = require('express-session')
+app.use(session({
+  secret: 'your secret key',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+// flash要有session才會啟動
+const flash = require('connect-flash')
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
 })
 
 app.use('/', require('./routes/home'))
--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const shortenUrlCheckAndStorage = require('../shortenUrlCheckAndStorage')
 
 const Url = require('../models/url')
 
@@ -14,6 +15,39 @@ router.get('/', (req, res) => {
       if (err) return console.error(err)
       res.render('index', { urls, mainUrl })
     })
+})
+
+router.post('/', (req, res) => {
+  console.log('req.body', req.body)
+  if (req.body.input === "") {
+    // req.body.input === "" 代表使用者沒有輸入內容
+    // 若使用者沒有輸入內容，就按下了送出鈕，需要防止表單送出並提示使用者
+    req.flash('warning_msg', '請輸入網址！')
+    res.redirect('/')
+  } else {
+    Url.find()
+      .lean()
+      .exec((err, urls) => {
+        // 若需要防止有重覆的網址組合出現
+        // 先列印出所有縮網址給shortenUrlCheckAndStorage檢查
+        // shortenUrlCheckAndStorage會給予不重複的縮網址
+        const paramsList = []
+        for (let i = 0; i < urls.length; i++) {
+          paramsList.push(urls[i].paramsUrl)
+        }
+        shortenUrlCheckAndStorage(paramsList)
+        const newParams = paramsList.pop()
+        const newUrl = new Url({
+          inputUrl: req.body.input,
+          paramsUrl: newParams
+        })
+        newUrl.save(err => {
+          if (err) return console.error(err)
+          req.flash('success_msg', '成功產生縮網址！')
+          res.redirect('/')
+        })
+      })
+  }
 })
 
 module.exports = router
